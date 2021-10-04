3f30b33f36728bd88aa37f199155d7e4918a2d41
Hector Gonzalez <hgonzalezmata@gmail.com>
2021-10-04 13:53:43 -0400

    Deshabilitado el buffer scratch

--- init.el
+++ init.el
@@ -4,8 +4,8 @@
 (add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 ;; (package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; (unless package-archive-contents
+;;   (package-refresh-contents))
 
 ;; Opciones de rendimiento
 
@@ -13,26 +13,6 @@
 
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
-
-;;Deja el buffer scratch vacio
-(setq initial-scratch-message "")
-
-;; Elimina *scratch* del buffer despues de que el modo se ha elegido
-(defun remove-scratch-buffer ()
-  (if (get-buffer "*scratch*")
-      (kill-buffer "*scratch*")))
-(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
-
-;; Elimina los mensajes del buffer
-(setq-default message-log-max nil)
-(kill-buffer "*Messages*")
-
-;;Elimina *Completions* del buffer despues de que se abre el archivo
-(add-hook 'minibuffer-exit-hook
-	  '(lambda ()
-             (let ((buffer "*Completions*"))
-               (and (get-buffer buffer)
-                    (kill-buffer buffer)))))
 
 ;; Don't show *Buffer list* when opening multiple files at the same time.
 (setq inhibit-startup-buffer-menu t)
@@ -42,6 +22,9 @@
 
 ;; Show only one active window when opening multiple files at the same time.
 (add-hook 'window-setup-hook 'delete-other-windows)
+
+;;Deshbilitado el buffer scratch
+(setq initial-major-mode (quote fundamental-mode))
 
 ;;Use-package
 
