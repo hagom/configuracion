ab5510cdcefbdbaeac3ee5373b00a06884e4968d
Hector Gonzalez <hgonzalezmata@gmail.com>
2021-10-10 21:11:40 -0400

    Agregados varios cambios

--- init.el
+++ init.el
@@ -16,6 +16,9 @@
 ;; The default is 800 kilobytes.  Measured in bytes.
 (setq gc-cons-threshold (* 50 1000 1000))
 
+;; Para crear archivos de respaldo ~
+(setq make-backup-files nil) 
+
 ;; Essential settings.
 (setq inhibit-splash-screen t
       inhibit-startup-message t
@@ -28,7 +31,10 @@
 (fset 'yes-or-no-p 'y-or-n-p)
 
 ;; Show only one active window when opening multiple files at the same time.
-(add-hook 'window-setup-hook 'delete-other-windows)
+;; (add-hook 'window-setup-hook 'delete-other-windows)
+
+;; Muestra los niveles de indentacion con colores
+(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
 
 ;;Use-package
 
@@ -64,7 +70,7 @@
  '(custom-safe-themes
    '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))
  '(package-selected-packages
-   '(electric-operator ti smart-newline indent-guide highlight-indent-guides elfeed ggtags rg smart-mode-line color-theme impatient-mode emmet-mode yaml-mode beacon git-timemachine git-gutter projectile flycheck powerline evil-collection evil treemacs-magit treemacs-icons-dired origami auto-rename-tag treemacs-evil treemacs-all-the-icons json-reformat lsp-mode magit pdf-tools django-snippets django-mode rainbow-delimiters dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui company-wordfreq company-org-block company-phpactor company-php company-ansible T org-roam engine-mode emojify org2blog org-wc languagetool apache-mode counsel ox-publish elpy company-tabnine all-the-icons-dired all-the-icons-ivy all-the-icons fzf treemacs-projectile treemacs neotree-toggle smartparens tern-auto-complete tern js2-refactor ac-js2 web-mode multiple-cursors hungry-delete ace-window org-bullets use-package magit-popup web-search org-web-tools powerthesaurus org-alert org-review evil-args evil-commentary evil-mc evil-mc-extras evil-nerd-commenter evil-org evil-surround airline-themes powerline-evil pandoc-mode tss typescript-mode import-js js2-mode node-resolver npm-mode github-search magit-circleci magit-lfs magit-org-todos magit-rbr magit-reviewboard magit-todos magit-vcsh orgit org-ac org-context org-evil org-jira org-kanban org-multi-wiki org-preview-html org-sidebar org-sync weechat weechat-alert viking-mode captain seq yasnippet auto-virtualenv indent-tools lsp-jedi pony-mode pydoc pylint python-mode python-pytest 2048-game composer flycheck-phpstan flymake-phpcs php-mode php-refactor-mode php-runtime phpactor phpunit smarty-mode async-await bpr concurrent ac-emmet yasnippet-classic-snippets xclip which-key websocket web-server undo-tree transcribe svg-lib svg-clock sql-indent scanner rainbow-mode python poker phps-mode orgalist org-translate org-edna ivy-hydra gnu-elpa-keyring-update gnu-elpa flymake-proselint eldoc-eval el-search eglot dict-tree csv-mode company-statistics company-ebdb cobol-mode chess auto-correct async aggressive-indent)))
+   '(jedi-core electric-operator ti highlight-indent-guides elfeed ggtags rg color-theme impatient-mode emmet-mode yaml-mode beacon git-timemachine git-gutter projectile flycheck powerline evil-collection evil treemacs-magit treemacs-icons-dired origami auto-rename-tag treemacs-evil treemacs-all-the-icons json-reformat lsp-mode magit pdf-tools django-snippets django-mode rainbow-delimiters dap-mode lsp-treemacs lsp-ivy helm-lsp lsp-ui company-wordfreq company-org-block company-phpactor company-php company-ansible T org-roam engine-mode emojify org2blog org-wc languagetool apache-mode counsel ox-publish elpy company-tabnine all-the-icons-dired all-the-icons-ivy all-the-icons fzf treemacs-projectile treemacs neotree-toggle smartparens tern-auto-complete tern js2-refactor ac-js2 web-mode multiple-cursors hungry-delete ace-window org-bullets use-package magit-popup web-search org-web-tools powerthesaurus org-alert org-review evil-args evil-commentary evil-mc evil-mc-extras evil-nerd-commenter evil-org evil-surround airline-themes powerline-evil pandoc-mode tss typescript-mode import-js js2-mode node-resolver npm-mode github-search magit-circleci magit-lfs magit-org-todos magit-rbr magit-reviewboard magit-todos magit-vcsh orgit org-ac org-context org-evil org-jira org-kanban org-multi-wiki org-preview-html org-sidebar org-sync weechat weechat-alert viking-mode captain seq yasnippet auto-virtualenv indent-tools lsp-jedi pony-mode pydoc pylint python-mode python-pytest 2048-game composer flycheck-phpstan flymake-phpcs php-mode php-refactor-mode php-runtime phpactor phpunit smarty-mode async-await bpr concurrent ac-emmet yasnippet-classic-snippets xclip which-key websocket web-server undo-tree transcribe svg-lib svg-clock sql-indent scanner rainbow-mode python poker phps-mode orgalist org-translate org-edna ivy-hydra gnu-elpa-keyring-update gnu-elpa flymake-proselint eldoc-eval el-search eglot dict-tree csv-mode company-statistics company-ebdb cobol-mode chess auto-correct async aggressive-indent)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -101,7 +107,7 @@
             (ibuffer-switch-to-saved-filter-groups "default")))
 
 ;; don't show these
-                                        ;(add-to-list 'ibuffer-never-show-predicates "zowie")
+;;(add-to-list 'ibuffer-never-show-predicates "zowie")
 ;; Don't show filter groups if there are no buffers in that group
 (setq ibuffer-show-empty-filter-groups nil)
 
@@ -207,6 +213,11 @@
 	      (yas-activate-extra-mode 'fundamental-mode)
 	      )
 	    )
+  :config
+  (setq yas-prompt-functions '(yas-dropdown-prompt                                                                                         
+			       yas-ido-prompt
+			       yas-completing-prompt)
+	)
   )
 
 ;;Editar multiples regiones al mismo tiempo
@@ -549,6 +560,11 @@ _l_: last hunk        set start _R_evision
   (setq ivy-display-style 'fancy)
   )
 
+(use-package ivy-prescient
+  :after counsel
+  :config (ivy-prescient-mode 1)
+  )
+
 (use-package swiper
   :ensure t
   :bind (
@@ -812,6 +828,7 @@ _l_: last hunk        set start _R_evision
 (use-package python-mode
   :ensure t
   :hook (python-mode . lsp-deferred)
+  :mode "\\.py\\'"
   :custom
   (python-shell-interpreter "python3"))
 
@@ -845,14 +862,12 @@ _l_: last hunk        set start _R_evision
 	)
   )
 
-;; Permite colorear la indentacion del codigo
-(use-package highlight-indent-guides
-  :ensure t
-  :config
-  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
-  (set-face-background 'highlight-indent-guides-odd-face "darkgray")
-  (set-face-background 'highlight-indent-guides-even-face "dimgray")
-  (set-face-foreground 'highlight-indent-guides-character-face "dimgray")
-  )
+;;Autocompletado para python
+(use-package jedi-core
+  :ensure t)
+
+;;Conexiones remotas desde emacs
+(use-package tramp
+  :ensure t)
 
 ;;; init.el ends here
