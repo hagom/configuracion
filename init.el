c92dc74113859531cfa897c1720fb38111b2e6b5
Hector Gonzalez <hgonzalezmata@gmail.com>
2021-11-08 15:16:35 -0400

    Integración de yasnippets con company

--- init.el
+++ init.el
@@ -281,11 +281,40 @@
 	      )
 	    )
   :config
-  (setq yas-prompt-functions '(yas-dropdown-prompt                                                                                         
+  (setq yas-prompt-functions '(yas-dropdown-prompt
 			       yas-ido-prompt
 			       yas-completing-prompt)
 	)
   )
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "->") t nil)
+	)
+      )
+    )
+  )
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand))
+  )
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
+(global-set-key ( kbd "s-z" ) 'tab-indent-or-complete)
 
 (use-package yasnippet-snippets
   :ensure t
