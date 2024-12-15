100649831162d326c25470c52d53730cddb80d0d
Hector Gonzalez <hgonzalezmata@gmail.com>
2024-12-15 03:20:20 -0400

    Eliminado neodev de la configuracion
    Se elimina neodev y se reemplaza por lazydev

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -4,6 +4,20 @@ return {
 	branch = "master",
 	dependencies = {
 		"williamboman/mason.nvim",
-		"folke/neodev.nvim",
-	}
+		{
+			"folke/lazydev.nvim",
+			ft = "lua", -- only load on lua files
+			opts = {
+				library = {
+					-- See the configuration section for more details
+					-- Load luvit types when the `vim.uv` word is found
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
+	config = function()
+		require 'lspconfig'.lua_ls.setup {}
+	end
+
 }
