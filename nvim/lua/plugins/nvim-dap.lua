e8fd1c6a54a7dd382dbb916cd26c9fe6b3a6d1d1
Hector Gonzalez <hgonzalezmata@gmail.com>
2025-01-27 01:01:51 -0400

    Agregado nvim-dap

--- nvim/lua/plugins/nvim-dap.lua
+++ nvim/lua/plugins/nvim-dap.lua
@@ -1,18 +1,87 @@
+
 return {
-  "mfussenegger/nvim-dap",
-  dependencies = {
-    "theHamsta/nvim-dap-virtual-text",
-    "rcarriga/nvim-dap-ui",
-    opts = 
-    {
-      event = "VeryLazy",
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+	  "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "mfussenegger/nvim-dap-python",
+      "theHamsta/nvim-dap-virtual-text",
     },
-    "leoluz/nvim-dap-go",
-    "mxsdev/nvim-dap-vscode-js",
-    -- "anuvyklack/hydra.nvim",
-    "nvim-telescope/telescope-dap.nvim",
-    "rcarriga/cmp-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      local dap_python = require("dap-python")
+
+      require("dapui").setup({})
+      require("nvim-dap-virtual-text").setup({
+        commented = true, -- Show virtual text alongside comment
+      })
+
+      dap_python.setup("python3")
+
+      vim.fn.sign_define("DapBreakpoint", {
+        text = "",
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+
+      vim.fn.sign_define("DapBreakpointRejected", {
+        text = "", -- or "❌"
+        texthl = "DiagnosticSignError",
+        linehl = "",
+        numhl = "",
+      })
+
+      vim.fn.sign_define("DapStopped", {
+        text = "", -- or "→"
+        texthl = "DiagnosticSignWarn",
+        linehl = "Visual",
+        numhl = "DiagnosticSignWarn",
+      })
+
+      -- Automatically open/close DAP UI
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+
+      local opts = { noremap = true, silent = true }
+
+      -- Toggle breakpoint
+      vim.keymap.set("n", "<leader>db", function()
+        dap.toggle_breakpoint()
+      end, opts)
+
+      -- Continue / Start
+      vim.keymap.set("n", "<leader>dc", function()
+        dap.continue()
+      end, opts)
+
+      -- Step Over
+      vim.keymap.set("n", "<leader>do", function()
+        dap.step_over()
+      end, opts)
+
+      -- Step Into
+      vim.keymap.set("n", "<leader>di", function()
+        dap.step_into()
+      end, opts)
+
+      -- Step Out
+      vim.keymap.set("n", "<leader>dO", function()
+        dap.step_out()
+      end, opts)
+			
+      -- Keymap to terminate debugging
+	  vim.keymap.set("n", "<leader>dq", function()
+	      require("dap").terminate()
+      end, opts)
+
+      -- Toggle DAP UI
+      vim.keymap.set("n", "<leader>du", function()
+        dapui.toggle()
+      end, opts)
+    end,
   },
-  branch = "master",
-  keys = { { "<leader>db", desc = "Open Debug menu" } },
 }
